61e125bc1911433507ca51821c4bc4add4205ffc
RaphaMABDF <raphael.martins.nu@gmail.com>
2026-08-08 15:59:05 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,14 +2,14 @@ window.CATALOG_CONFIG = {
   // 1) Cole aqui o ID da sua planilha Google Sheets.
   // Exemplo de URL: https://docs.google.com/spreadsheets/d/1ABCdefGHIjklMNopQRstuVWxyz/edit
   // O ID é: 1ABCdefGHIjklMNopQRstuVWxyz
-  SHEET_ID: 'COLE_AQUI_O_ID_DA_SUA_PLANILHA',
+  SHEET_ID: '1t7kMSSi92wEX3p7hOFaY9CCM3h8bk9O8',
 
   // 2) Nome exato da aba do Google Sheets.
   SHEET_TAB: 'ESTOQUE_MESTRE',
 
   // 3) Seu WhatsApp no padrão internacional. Brasil: 55 + DDD + número.
   // Exemplo: 5561999999999
-  WHATSAPP_NUMBER: '5561999999999',
+  WHATSAPP_NUMBER: '5561986186556',
 
   // 4) Endpoint público para transformar Google Sheets em JSON.
   OPENSHEET_BASE: 'https://opensheet.elk.sh'
